735218b672767002f6fe46f0ab24812d3d4aed0b
JohnDurand33 <142426904+JohnDurand33@users.noreply.github.com>
2024-06-10 07:56:34 -0600

    Both org email fetch types work independantly allowing HomeDog and SearchDog objects to function interactively with no issues. Token updates by the minute functional with token required wrappers where necessary

--- app/api/routes.py
+++ app/api/routes.py
@@ -7,6 +7,9 @@ from .__init__ import api
 from sqlalchemy.exc import SQLAlchemyError
 from ..DogMatcher import DogMatcher
 import random
+import requests
+import json
+import os
 
 
 @api.route('/getbreeds', methods=['GET'])
@@ -21,6 +24,21 @@ def get_fav_breeds(user):
         print(f'Error: {e}')
         return jsonify({'message': 'Internal Server Error'}), 500
 
+
+@api.route('/get_favdogs', methods=["GET", "POST"])
+@token_required
+def get_favorite_dogs(user):
+    try:
+        fav_dogs = db.session.query(Dog).join(
+            fav_dog).filter_by(user_id=user.id).all()
+        return dogs_schema.jsonify(fav_dogs), 200
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        print(f"Database error: {e}")
+        return jsonify({"error": "Database error"}), 500
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": "Error getting favorite dogs"}), 500
 
 @api.route('/updatebreeds', methods=['POST'])
 @token_required
@@ -86,18 +104,19 @@ def add_favorite_dog(user):
         return jsonify({"error": "Error adding favorite dog"}), 500
 
 
-@api.route('/rem_favdog/<int:dog_id>', methods=['DELETE'])
+@api.route('/rem_favdog/<string:api_id>', methods=["DELETE"])
 @token_required
-def delete_favorite_dog(user, dog_id):
+def delete_favorite_dog(user, api_id):
     try:
-        # Check if the dog exists in the favorites
+
         existing_entry = db.session.query(fav_dog).filter_by(
-            user_id=user.id, dog_id=dog_id).first()
+            user_id=user.id, dog_id=api_id).first()
+
         if not existing_entry:
             return jsonify({"error": "Dog not found in favorites"}), 404
 
         db.session.query(fav_dog).filter_by(
-            user_id=user.id, dog_id=dog_id).delete()
+            user_id=user.id, dog_id=api_id).delete()
         db.session.commit()
 
         return jsonify({"message": "Dog removed from favorites"}), 200
@@ -127,6 +146,48 @@ def clear_fav_dogs(user):
     except Exception as e:
         print(f"Error: {e}")
         return jsonify({"error": "Error deleting favorite dog"}), 500
+    
+@api.route('/get_org_details/<string:dog_id>', methods=['GET'])
+@token_required
+def get_org_details(user, dog_id):
+    try:
+        dog = Dog.query.filter_by(api_id=dog_id).first()
+        if not dog:
+            return jsonify({"error": "Dog not found"}), 404
+
+        org_id = dog.org_id
+        payload = {
+            "apikey": os.getenv('RESCUE_KEY'),
+            "objectType": "orgs",
+            "objectAction": "publicSearch",
+            "search": {
+                "resultLimit": 1,
+                "filters": [
+                    {"fieldName": "orgID", "operation": "equals", "criteria": org_id}
+                ],
+                "fields": ["orgID", "orgEmail"]
+            }
+        }
+        url = 'https://api.rescuegroups.org/http/v2.json'
+        headers = {'Content-Type': 'application/json'}
+
+        response = requests.post(
+            url=url, headers=headers, data=json.dumps(payload))
+        response.raise_for_status()
+
+        org_data = response.json().get('data', {})
+        if not org_data:
+            return jsonify({"error": "Organization not found"}), 404
+
+        org_email = next(iter(org_data.values())).get('orgEmail')
+        if not org_email:
+            return jsonify({"error": "Organization email not found"}), 404
+
+        return jsonify({"orgEmail": org_email}), 200
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"message": "Internal Server Error"}), 500
+    
 
 
 
